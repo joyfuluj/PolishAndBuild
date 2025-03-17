d0f5ac89a9396671dc8a665f454248072e89bd9e
NamanArora332 <aroranaman116@gmail.com>
2025-03-17 15:53:16 -0700

    Complete Score UI

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
     [SerializeField] private int maxLives = 3;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+    [SerializeField] private ScoreUI scoreUI;
 
     private int currentBrickCount;
     private int totalBrickCount;
+    private int score;
 
     private void OnEnable()
     {
@@ -15,6 +18,8 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         ball.ResetBall();
         totalBrickCount = bricksContainer.childCount;
         currentBrickCount = bricksContainer.childCount;
+        score = 0; // Initialize score
+        scoreUI.UpdateScore(score); // Set initial score
     }
 
     private void OnDisable()
@@ -27,15 +32,26 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         ball.FireBall();
     }
 
-    public void OnBrickDestroyed(Vector3 position)
+   
+        public void OnBrickDestroyed(Vector3 position)
     {
         // fire audio here
         // implement particle effect here
         // add camera shake here
+        score++;
+        if (scoreUI != null)
+        {
+            scoreUI.UpdateScore(score); // Update the UI safely
+        }
+        else
+        {
+            Debug.Log("ScoreUI is not assigned in GameManager!");
+        }
         currentBrickCount--;
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
-        if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
+        if (currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
     }
+
 
     public void KillBall()
     {
